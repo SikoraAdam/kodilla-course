ed46f6f4f8f6ae2c88e786c627ab75a4c519e24d
SikoraAdam <sikora.adam@mail.com>
2018-08-17 23:26:58 +0200

    Module_7.3 v1

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,11 +1,34 @@
 package com.kodilla.stream;
 
+/*
 import com.kodilla.stream.beautifier.PoemBeautifier;
 import com.kodilla.stream.lambda.ExpressionExecutor;
 import com.kodilla.stream.reference.FunctionalCalculator;
+*/
+import com.kodilla.stream.forumuser.*;
+
+import java.time.*;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class StreamMain {
     public static void main(String[] args) {
+        Forum theForum = new Forum();
+
+        Map<Integer, ForumUser> theResultMapOfUsers = theForum.getUserList().stream()
+                .filter(user -> user.getUserSex() == 'M')
+                .filter(user -> (Period.between(user.getUserBirthDate(), LocalDate.now()).getYears()) > 20)
+                .filter(user -> user.getUserPostsQuantity() > 0)
+                .collect(Collectors.toMap(ForumUser::getUserId, user -> user));
+
+        System.out.println("# elements: " + theResultMapOfUsers.size());
+        theResultMapOfUsers.entrySet().stream()
+                .map(entry -> entry.getKey() + ": " + entry.getValue().getUserName() + ", " + entry.getValue().getUserSex() + ", " + entry.getValue().getUserBirthDate() + ", " + entry.getValue().getUserPostsQuantity())
+                .forEach(System.out::println);
+
+
+        //Module_7.1 + Module_7.2
+        /*
         ExpressionExecutor expressionExecutor = new ExpressionExecutor();
         PoemBeautifier poemBeautifier = new PoemBeautifier();
 
@@ -26,5 +49,6 @@ public class StreamMain {
         poemBeautifier.beautify("porzeczka", (porzeczka) -> porzeczka.replace('p', 'P'));
         poemBeautifier.beautify("melon", (melon) -> melon.substring(2,4));
         poemBeautifier.beautify("kosmos", (kosmos) ->kosmos.trim());
+        */
     }
 }
