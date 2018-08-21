5a7414936d4b1d3110cd3c3605cba9f18e965bba
SikoraAdam <sikora.adam@mail.com>
2018-08-21 12:05:27 +0200

    Temp

--- kodilla-exception/src/main/java/com/kodilla/exception/io/FileReader.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/io/FileReader.java
@@ -1,9 +1,11 @@
 package com.kodilla.exception.io;
 
 import java.io.File;
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.sql.SQLOutput;
 import java.util.stream.Stream;
 
 public class FileReader {
@@ -11,6 +13,12 @@ public class FileReader {
         ClassLoader classLoader = getClass().getClassLoader();
         File file = new File(classLoader.getResource("file/names.txt").getFile());
         Path path = Paths.get(file.getPath());
-        //Stream<String> fileLines = Files.lines(path);
+        try (Stream<String> fileLines = Files.lines(path)){
+            fileLines.forEach(System.out::println);
+        } catch (IOException e) {
+            System.out.println(e);
+        } finally {
+            System.out.println("bye");
+        }
     }
 }
