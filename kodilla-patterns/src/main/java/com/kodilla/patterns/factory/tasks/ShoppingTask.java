c893134f6b6cde6ad4fee23c165cf51db5a51b2d
Cross <C%3Rwien>
2018-09-10 12:16:03 +0200

    Module_11.4

---
+++ kodilla-patterns/src/main/java/com/kodilla/patterns/factory/tasks/ShoppingTask.java
@@ -0,0 +1,25 @@
+package com.kodilla.patterns.factory.tasks;
+
+public class ShoppingTask {
+    private String taskName;
+    private String whatToBuy;
+    private double quantity;
+
+    public ShoppingTask(String taskName, String whatToBuy, double quantity) {
+        this.taskName = taskName;
+        this.whatToBuy = whatToBuy;
+        this.quantity = quantity;
+    }
+
+    public String getTaskName() {
+        return taskName;
+    }
+
+    public String getWhatToBuy() {
+        return whatToBuy;
+    }
+
+    public double getQuantity() {
+        return quantity;
+    }
+}
