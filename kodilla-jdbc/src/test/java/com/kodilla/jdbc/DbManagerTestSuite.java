167344daedb9b9ad2920998bf7ca7cc73dadcaf6
Cross <C%3Rwien>
2018-09-14 15:30:23 +0200

    jdbc

--- kodilla-jdbc/src/test/java/com/kodilla/jdbc/DbManagerTestSuite.java
+++ kodilla-jdbc/src/test/java/com/kodilla/jdbc/DbManagerTestSuite.java
@@ -1,8 +1,7 @@
 package com.kodilla.jdbc;
 
-import org.junit.Assert;
-import org.junit.Test;
-import java.sql.SQLException;
+import org.junit.*;
+import java.sql.*;
 
 public class DbManagerTestSuite {
     @Test
@@ -12,5 +11,28 @@ public class DbManagerTestSuite {
         DbManager dbManager = DbManager.getInstance();
         //Then
         Assert.assertNotNull(dbManager.getConnection());
+    }
+
+    @Test
+    public void testSelectUsers() throws SQLException {
+        //Given
+        DbManager dbManager = DbManager.getInstance();
+
+        //When
+        String sqlQuery = "SELECT * FROM USERS";
+        Statement statement = dbManager.getConnection().createStatement();
+        ResultSet rs = statement.executeQuery(sqlQuery);
+
+        //Then
+        int counter = 0;
+        while(rs.next()) {
+            System.out.println(rs.getInt("ID") + ", " +
+                    rs.getString("FIRSTNAME") + ", " +
+                    rs.getString("LASTNAME"));
+            counter++;
+        }
+        rs.close();
+        statement.close();
+        Assert.assertEquals(4, counter);
     }
 }
