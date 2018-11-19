220fa6bcde1ef363cddfc660d6920e9d4177821e
SikoraAdam <sikora.adam@mail.com>
2018-11-19 15:20:25 +0100

    Module_22.3

--- kodilla-stream/src/test/java/com/kodilla/stream/invoice/simple/SimpleInvoiceTestSuite.java
+++ kodilla-stream/src/test/java/com/kodilla/stream/invoice/simple/SimpleInvoiceTestSuite.java
@@ -15,4 +15,23 @@ public class SimpleInvoiceTestSuite {
         //Then
         Assert.assertEquals(108.975, invoice.getValueToPay(), 0.001);
     }
+
+    @Test
+    public void getValueTest() {
+        //Given
+        SimpleItem item1 = new SimpleItem(new SimpleProduct("Product 1", 15.00), 1.00);
+        SimpleItem item2 = new SimpleItem(new SimpleProduct("Product 2", 15.00), 2.00);
+        SimpleItem item3 = new SimpleItem(new SimpleProduct("Product 3", 15.00), 3.00);
+
+        //When
+        double result1 = item1.getValue();
+        double result2 = item2.getValue();
+        double result3 = item3.getValue();
+
+        //Then
+        Assert.assertEquals(15.00, result1, 0);
+        Assert.assertEquals(30.00, result2, 0);
+        Assert.assertEquals(45.00, result3, 0);
+
+    }
 }
